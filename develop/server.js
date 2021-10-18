d70982ac088aaa267fc4b9d3e2fd7703f4a8215e
NazikBest Coder <n.askarbekkyzy@gmail.com>
2021-10-18 09:38:35 -0400

    server

--- develop/server.js
+++ develop/server.js
@@ -1,4 +1,10 @@
 const express = require("express");
+const fs = require("fs");
+const notes = require("./db/db.json");
+const path = require("path");
+const uuid = require("uuid");
+const { DH_CHECK_P_NOT_SAFE_PRIME } = require("constants");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -13,5 +19,22 @@ app.use("/", htmlRoutes);
 app.listen(PORT, () => {
     console.log(`listening on ${PORT}`)
 });
+
+
+// const express = require("express");
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+// const htmlRoutes = require("./routes/htmlR.js");
+
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static('public'));
+// app.use("/", htmlRoutes);
+
+// app.listen(PORT, () => {
+//     console.log(`listening on ${PORT}`)
+// });
 
 
