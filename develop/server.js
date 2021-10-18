b3c4933b61fa5c7f36789487d9e0d1a3ee0e0afe
NazikBest Coder <n.askarbekkyzy@gmail.com>
2021-10-18 09:42:53 -0400

    add notes apis

--- develop/server.js
+++ develop/server.js
@@ -15,7 +15,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use("/", htmlRoutes);
-
+// Connect API Routes
+// Saves notesinto db.json
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"))
+});
+// Add notes
+app.post("/api/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const newNotes = req.body;
+    newNotes.id = uuid.v4();
+    notes.push(newNotes);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+    res.json(notes);
+});
 app.listen(PORT, () => {
     console.log(`listening on ${PORT}`)
 });
