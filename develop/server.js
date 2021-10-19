a01fe281cd4dee4905071c6f9e06713ea1735836
NazikBest Coder <n.askarbekkyzy@gmail.com>
2021-10-18 23:05:05 -0400

    uuid

--- develop/server.js
+++ develop/server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const fs = require("fs");
 const notes = require("./db/db.json");
 const path = require("path");
-const uuid = require("uuid");
+const { v4: uuidv4 } = require('uuid');
 const { DH_CHECK_P_NOT_SAFE_PRIME } = require("constants");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 const htmlRoutes = require("./routes/htmlR.js");
 
@@ -18,24 +18,32 @@ app.use("/", htmlRoutes);
 // Connect API Routes
 // Saves notesinto db.json
 app.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/db/db.json"))
+    res.sendFile(path.join(__dirname, "../db/db.json"))
 });
 // Add notes
 app.post("/api/notes", (req, res) => {
-    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const notes = JSON.parse(fs.readFileSync("../db/db.json"));
     const newNotes = req.body;
     newNotes.id = uuid.v4();
     notes.push(newNotes);
-    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+    fs.writeFileSync("../db/db.json", JSON.stringify(notes))
     res.json(notes);
 });
 // Deletes notes
 app.delete("/api/notes/:id", (req, res) => {
-    const notes = JSON.parse(fs.readFyleSync("./db/db.json"));
+    const notes = JSON.parse(fs.readFyleSync("../db/db.json"));
     const delNote = notes.filter((rmvNote) =>rmvNote.id !== req.params.id);
-    fs.writeFileSync("./db/db/json", JSON.stringify(delNote));
+    fs.writeFileSync("../db/db/json", JSON.stringify(delNote));
     res.json(delNote);
 })
+// Index.html call
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+// Notes.html call
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
 app.listen(PORT, () => {
     console.log(`listening on ${PORT}`)
 });
